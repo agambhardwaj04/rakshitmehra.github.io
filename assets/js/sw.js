9c2ca6b741db6213b9a6498150eead09263a292d
rakshitmehra <rakshitmehra88@gmail.com>
2023-01-19 17:45:01 +0530

    json try

---
+++ assets/js/sw.js
@@ -0,0 +1,32 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('Service worker registration successful:', registration.scope);
+  }).catch(function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+}
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('mypwa-static-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/main.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
